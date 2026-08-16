ccbedc8b0ffd5934fa1e7354b33512683a2090d0
USER81690 <fnvr29hbwh@privaterelay.appleid.com>
2026-08-17 00:02:23 +0300

    Adding main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from "vue"
 import App from "./App.vue"
+import store from "./store"
 import vuetify from "./plugins/vuetify"
+import '@mdi/font/css/materialdesignicons.css'
 
 
 const app = createApp(App)
+app.use(store)
 app.use(vuetify)
 app.mount("#app")
