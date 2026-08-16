f5357caa407c54c3903f7a87050159743f0ff48b
USER81690 <fnvr29hbwh@privaterelay.appleid.com>
2026-08-17 00:00:28 +0300

    Add actions for CRUD operations

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,4 +1,6 @@
 import { createStore } from "vuex"
+import axios from "axios"
+
 
 export default createStore({
     state: {
@@ -51,6 +53,52 @@ export default createStore({
         },
     },
 
-    
-    actions: {}, 
+
+    actions: {
+        async fetchArticles({ commit }) {
+            const res = await axios.get("/articles/")
+            commit("SET_ARTICLES", res.data)
+        },
+        async createArticle({ commit }, data) {
+            const res = await axios.post("/article/", data)
+            commit("ADD_ARTICLE", res.data)
+            return res.data
+        },
+        async updateArticle({ commit }, data) {
+            const res = await axios.patch(`/article/${data.id}/`, data)
+            commit("UPDATE_ARTICLE", res.data)
+            return res.data
+        },
+        async deleteArticle({ commit }, id) {
+            await axios.delete(`/article/${id}/`)
+            commit("DELETE_ARTICLE", id)
+        },
+        async fetchComments({ commit }, articleId) {
+            const res = await axios.get(`/article/${articleId}/comments/`)
+            commit("SET_COMMENTS_FOR_ARTICLE", {
+                articleId,
+                comments: res.data,
+            })
+            return res.data
+        },
+        async createComment({ commit }, { articleId, text }) {
+            const res = await axios.post(`/article/${articleId}/comment/`, {
+                text,
+            })
+            commit("ADD_COMMENT", { articleId, comment: res.data })
+            return res.data
+        },
+        async updateComment({ commit }, { articleId, id, text }) {
+            const res = await axios.patch(
+                `/article/${articleId}/comment/${id}/`,
+                { text },
+            )
+            commit("UPDATE_COMMENT", { articleId, comment: res.data })
+            return res.data
+        },
+        async deleteComment({ commit }, { articleId, commentId }) {
+            await axios.delete(`/article/${articleId}/comment/${commentId}/`)
+            commit("DELETE_COMMENT", { articleId, commentId })
+        },
+    },
 })
