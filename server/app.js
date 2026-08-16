a32183deb8078b143f833a54d6e59a75a8436705
USER81690 <fnvr29hbwh@privaterelay.appleid.com>
2026-08-16 19:50:41 +0300

    Set up database connection and refactor server startup

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import "dotenv/config"
 import express from "express"
+import sequelize from "./sequelize.js"
 
 const PORT = process.env.PORT
 const app = express()
@@ -8,6 +9,18 @@ const app = express()
 app.get("/", (req, res) => {res.send('The server is up')})
 
 
-app.listen(PORT, () => {
-    console.log(`The server is running on port: ${PORT}`)
-})
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        console.log("Connection to the DBMS")
+        app.listen(PORT, () => {
+            console.log(`The server is running on port: ${PORT}`)
+        })
+    } catch (error) {
+        if (error.name == "SequelizeConnectionError") {
+            console.log("Error when connecting to the DBMS")
+        } else {
+            console.log("Error during server startup")
+        }
+    }
+}; start()
